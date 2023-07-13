ae9963dd7b5b78fd8b1552b50ae172c1aba8fc54
benzsuankularb <benzsk130@gmail.com>
2023-07-13 11:05:29 +0700

    rename form fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { DatePicker_FormField } from "./components/actions/date-picker--form-field";
-import { EntityMultiPicker_FormField } from "./components/actions/entity-multi-picker--form-field";
-import { EntityPicker_FormField } from "./components/actions/entity-picker--form-field";
-import { ImageMultiUpload_FormField } from "./components/actions/image-multi-upload--form-field";
-import { ImageUpload_FormField } from "./components/actions/image-upload--form-field";
-import { LocalizeText_FormField } from "./components/actions/localize-text--form-field";
-import { Text_FormField } from "./components/actions/text--form-field";
-import { Toggle_FormField } from "./components/actions/toggle--form-field";
+import { DatePicker_FormField } from "./components/form-fields/date-picker--form-field";
+import { EntityMultiPicker_FormField } from "./components/form-fields/entity-multi-picker--form-field";
+import { EntityPicker_FormField } from "./components/form-fields/entity-picker--form-field";
+import { ImageMultiUpload_FormField } from "./components/form-fields/image-multi-upload--form-field";
+import { ImageUpload_FormField } from "./components/form-fields/image-upload--form-field";
+import { LocalizeText_FormField } from "./components/form-fields/localize-text--form-field";
+import { Text_FormField } from "./components/form-fields/text--form-field";
+import { Toggle_FormField } from "./components/form-fields/toggle--form-field";
 import { EntityEdit } from "./components/selectors/entity-edit";
 import { SchemaLayout } from "./framework/components/schema-layout";
 import { WireframeGeneratorConfig } from "./framework/contexts";
@@ -16,7 +16,6 @@ import { event_editMedia } from "./schemas/events/edit-media";
 import { event_editPeriod } from "./schemas/events/edit-period";
 import { event_editSaleRestrictions } from "./schemas/events/edit-sale-restrictions";
 import { event_editWebsite } from "./schemas/events/edit-website";
-
 import { content_eventEdit } from "./schemas/events/event-edit";
 
 function App() {
